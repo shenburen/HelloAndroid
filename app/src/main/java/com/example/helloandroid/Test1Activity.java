bd77b82eaf4067012ba3e5ca0d109e516f0f35c2
shenzhiqiang <593446878@qq.com>
2025-09-27 17:32:59 +0800

    activity跳转。

--- app/src/main/java/com/example/helloandroid/Test1Activity.java
+++ app/src/main/java/com/example/helloandroid/Test1Activity.java
@@ -6,47 +6,37 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ActivityResultLauncher<Intent> launcher;
+public class Test1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), (ActivityResult result) -> {
-            Intent res = result.getData();
-            Log.i("Test", res.getStringExtra("return"));
-        });
+        Intent intent = getIntent();
+        Log.i("Test", intent.getStringExtra("test1"));
+        Log.i("Test", intent.getStringExtra("test2"));
     }
 
-    public void gotoTest1(View view) {
-        Intent messageIntent = new Intent(this, Test1Activity.class);
+    public void gotoTest2(View view) {
+        Intent messageIntent = new Intent(this, Test2Activity.class);
         messageIntent.putExtra("test1", "123");
         messageIntent.putExtra("test2", "456");
         startActivity(messageIntent);
     }
 
-    public void gotoTest2(View view) {
-        Intent messageIntent = new Intent(this, Test2Activity.class);
-        messageIntent.putExtra("test1", "789");
-        messageIntent.putExtra("test2", "012");
-
-        launcher.launch(messageIntent);
+    public void returnMain(View view) {
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/helloandroid/Test2Activity.java
@@ -0,0 +1,40 @@
+package com.example.helloandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Test2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_test2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        Log.i("Test", intent.getStringExtra("test1"));
+        Log.i("Test", intent.getStringExtra("test2"));
+    }
+
+    public void returnMain(View view) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("return", "返回了");
+        setResult(Activity.RESULT_OK, resultIntent);
+
+        finish();
+    }
+}
